2e64800797d654ef62819257c8044ba9404b74c9
patchcablemgr <53905413+patchcablemgr@users.noreply.github.com>
2020-02-11 17:35:26 -0800

    SAN Extension

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/cert.pem ]; then
 	if [ ! -f /etc/ssl/private/key.pem ]; then
-		openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=Washington/L=Auburn/O=PCM/CN=patchcablemgr.com" -addext "subjectAltName = DNS:$SUBJECT_ALTERNATE_NAME" -keyout /etc/ssl/private/key.pem  -out /etc/ssl/certs/cert.pem
+		openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=US/ST=Washington/L=Auburn/O=PCM/CN=patchcablemgr.com" -addext "subjectAltName = DNS:$SUBJECT_ALTERNATE_NAME,IP:$SUBJECT_ALTERNATE_IP" -keyout /etc/ssl/private/key.pem  -out /etc/ssl/certs/cert.pem
 	fi
 fi
 
